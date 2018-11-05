a72471d72d12fee03b547ff871939bb4e1805aea
Daniel O'Leary <djolear@stanford.edu>
2018-11-04 16:52:23 -0800

    trying to get it to make story, needs work

--- ngrams/ngramsMain.py
+++ ngrams/ngramsMain.py
@@ -1,5 +1,5 @@
 import os.path
-
+import random
 
 
 def welcome():
@@ -29,13 +29,41 @@ def readFile(input, n):
 
     #build ngrams map
     for i in range(len(myList)):
-        dictValue = "";
-        for j in range(1, n + 1):
-            dictValue = dictValue + " " + myList[(i + j) % len(myList)];
-        myDict[myList[i]] = dictValue;
+        dictKey = myList[i];
+        for j in range(1, n - 1):
+            dictKey = dictKey + " " + myList[(i + j) % len(myList)];
+        newValue = myList[(i + n - 1) % len(myList)];
+        if dictKey in myDict:
+            curValue = myDict[dictKey];
+            if newValue not in curValue:
+                curValue.append(newValue);
+                myDict[dictKey] = curValue;
+        else:
+            newKey = [];
+            newKey.append(newValue);
+            myDict[dictKey] = newKey;
 
     return myDict;
 
+def generateStory(nGramDict, numWords):
+    myStory = [];
+    keys = list(nGramDict.keys());
+    randStartIndex = random.randint(1, len(keys));
+    randStart = keys[randStartIndex];
+    myStory.append(randStart)
+    currentGram = randStart;
+    print(currentGram)
+    for i in range(0, numWords - 1 - len(randStart)):
+       currentValue = nGramDict[currentGram]
+       newWordIndex = random.randint(0, len(currentValue));
+
+       newWord = currentValue[newWordIndex];
+       newWindow = currentGram[1:] + " " + newWord;
+       myStory.append(newWindow);
+
+
+
+    return myStory;
 
 
 
@@ -44,8 +72,11 @@ def main():
 
     welcome();
     fileLocation = getInput();
-    n = input("How many grams? ")
-    ngramDict = readFile(fileLocation, int(n));
-
+    nGrams = input("How many grams? ");
+    numWords = input("Number of words to generate (0 to quit)? ");
+    #if numWords = 0, quit
+    nGramDict = readFile(fileLocation, int(nGrams));
+    nGramStory = generateStory(nGramDict, int(numWords))
+    print(nGramStory)
 
 if __name__ == "__main__": main()
