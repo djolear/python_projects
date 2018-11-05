e62277870c4184b6f6c95ef944e8f881337b798e
Daniel O'Leary <djolear@stanford.edu>
2018-11-04 16:04:14 -0800

    made ngram dictionary

--- ngrams/ngramsMain.py
+++ ngrams/ngramsMain.py
@@ -8,25 +8,44 @@ def welcome():
 
 def getInput():
     fileName = input("Input file? ")
-    fileLocation = 'G:/My Drive/personal/cs_projects/python_projects/madlibs/textFiles/' + fileName
+    fileLocation = 'G:/My Drive/personal/cs_projects/python_projects/ngrams/textFiles/' + fileName
 
     while os.path.exists(fileLocation) == False:
         print("That file does not exist. Please try again.\n")
         fileName = input("Input file? ")
         fileLocation = 'G:/My Drive/personal/cs_projects/python_projects/ngrams/textFiles/' + fileName
 
-    textFile = open(fileLocation, "r");
-
+    return fileLocation
 
 def readFile(input, n):
     textFile = open(input, "r");
+    myList = [];
+    myDict = {};
+
+    #make list of all words
+    for line in textFile:
+        for word in line.split():
+            myList.append(word);
+
+    #build ngrams map
+    for i in range(len(myList)):
+        dictValue = "";
+        for j in range(1, n + 1):
+            dictValue = dictValue + " " + myList[(i + j) % len(myList)];
+        myDict[myList[i]] = dictValue;
+
+    return myDict;
+
+
+
 
 
 def main():
 
     welcome();
     fileLocation = getInput();
-    readFile(fileLocation);
+    n = input("How many grams? ")
+    ngramDict = readFile(fileLocation, int(n));
 
 
 if __name__ == "__main__": main()
