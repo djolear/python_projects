760f563467f7b2a0f0fd05d34c72df3e6ff4de50
Daniel O'Leary <djolear@stanford.edu>
2018-11-16 20:45:08 -0800

    initial commit

---
+++ grammarSolver/grammarSolverMain.py
@@ -0,0 +1,14 @@
+import os.path()
+import random
+
+
+
+def main():
+    filename = input("Grammar file name? ");
+    symbol = input("Symbol to generate (Enter to quit)? ");
+    numSymbols = input("How many to generate? ");
+
+
+
+
+if __name__ == "__main__": main()
